77f849add3a4e6b59cfc153e0e178d793342a519
Oleksandr Ulianytskyi <a.ulyanitsky@gmail.com>
2011-09-12 17:25:25 +0300

    Change directory to +current_path+ before run unicorn command and other fixes

--- lib/vlad/unicorn_common.rb
+++ lib/vlad/unicorn_common.rb
@@ -2,7 +2,7 @@ require 'vlad'
 
 module Vlad
   module Unicorn
-    VERSION = '2.1.0' #:nodoc:
+    VERSION = '2.1.1' #:nodoc:
 
     # Runs +cmd+ using sudo if the +:unicorn_use_sudo+ variable is set.
     def self.maybe_sudo(cmd)
@@ -33,13 +33,14 @@ end
 
 namespace :vlad do
   set :unicorn_command,     "unicorn"
-  set(:unicorn_config)      { "#{current_path}/config/unicorn.rb" }
+  set :unicorn_config,      "config/unicorn.rb"
   set :unicorn_use_sudo,    false
   set(:unicorn_pid)         { "#{shared_path}/pids/unicorn.pid" }
-  set :unicorn_env, rails_env || "development"
+  set(:unicorn_env)   { rails_env || "development" }
 
   def unicorn(opts = '')
-    cmd = "#{unicorn_command} -D -E #{unicorn_env} --config-file #{unicorn_config}"
+    cmd = "cd #{current_path};"
+    cmd << " #{unicorn_command} -D -E #{unicorn_env} --config-file #{unicorn_config}"
     cmd << " #{opts}"
     cmd
   end
--- vlad-unicorn.gemspec
+++ vlad-unicorn.gemspec
@@ -1,8 +1,10 @@
 # -*- encoding: utf-8 -*-
+$:.push File.expand_path("../lib", __FILE__)
+require "vlad/unicorn_common"
 
 Gem::Specification.new do |s|
   s.name        = "vlad-unicorn"
-  s.version     = "2.0.1"
+  s.version     = Vlad::Unicorn::VERSION
   s.authors     = ["Kevin R. Bullock", "Oleksandr Ulianytskyi"]
   s.email       = ["kbullock@ringworld.org", "a.ulyanitsky@gmail.com"]
   s.homepage    = "http://bitbucket.org/krbullock/vlad-unicorn/"
