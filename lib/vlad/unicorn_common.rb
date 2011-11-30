02eb84929e37b58a0c0af53c351dc98a0bc1d94b
Oleksandr Ulianytskyi <a.ulyanitsky@gmail.com>
2011-11-30 15:38:30 +0200

    Added support for custom signals when reload unicorn. New variable - :unicorn_reload_signals.

--- lib/vlad/unicorn_common.rb
+++ lib/vlad/unicorn_common.rb
@@ -13,13 +13,18 @@ module Vlad
       end
     end
 
-    def self.signal(sig = '0')
-      %(test -s "#{unicorn_pid}" && kill -#{sig} `cat "#{unicorn_pid}"`)
+    def self.signal(signals = ['0'])
+      cmd = [%(test -s "#{unicorn_pid}")]
+      signals.each do |sig|
+        cmd << %(kill -#{sig} `cat "#{unicorn_pid}"`)
+      end
+      cmd.join(" && ")
     end
 
+    # Use *:unicorn_reload_signals* variable for set custom signals.
     def self.start(opts = '')
-      cmd = signal('HUP')
-      cmd << %( || (#{unicorn_command} -D --config-file #{unicorn_config} #{opts}))
+      cmd = signal(unicorn_reload_signals)
+      cmd << %( || (cd #{current_path} && #{unicorn_command} -D -E #{unicorn_env} -c #{unicorn_config} #{opts}))
       maybe_sudo %(sh -c '#{cmd}')
     end
 
@@ -32,17 +37,12 @@ module Vlad
 end
 
 namespace :vlad do
-  set :unicorn_command,     "unicorn"
-  set(:unicorn_config)      { "#{current_path}/config/unicorn.rb" }
-  set :unicorn_use_sudo,    false
-  set(:unicorn_pid)         { "#{shared_path}/pids/unicorn.pid" }
-  set(:unicorn_env)   { rails_env || "development" }
-
-  def unicorn(opts = '')
-    cmd = "(cd #{current_path}; #{unicorn_command} -D -E #{unicorn_env} -c #{unicorn_config})"
-    cmd << " #{opts}"
-    cmd
-  end
+  set :unicorn_command,          "unicorn"
+  set(:unicorn_config)           { "#{current_path}/config/unicorn.rb" }
+  set :unicorn_use_sudo,         false
+  set(:unicorn_pid)              { "#{shared_path}/pids/unicorn.pid" }
+  set(:unicorn_env)              { rails_env || "development" }
+  set :unicorn_reload_signals,   ['HUP']
 
   desc "Stop the app servers"
   remote_task :stop_app, :roles => :app do
